e1e0a6c6db75ce1fe364580704a915d318d41695
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-10 16:16:07 +0200

    Use Loop class for simulation loop

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import curves
 from multicontact_api import ContactSequence
 from pyb_solo_simulator import pybullet_simulator
+from loop import Loop
 
 DT = 0.002
 
@@ -16,6 +17,46 @@ def camera_follow(root):
     z = current_root[2]
     pyb.resetDebugVisualizerCamera(cameraDistance=state[-2], cameraYaw=state[-4], cameraPitch=state[-3],
                                    cameraTargetPosition=[x, y, z])
+
+
+
+class SimulatorLoop(Loop):
+    """
+    Class used to call pybullet at a given frequency
+    """
+    def __init__(self, period, q_t, dq_t):
+        """
+        Constructor
+        :param period: the time step between each new frame
+        :param q_t: the joint position trajectory, stored in a Curves object
+        :param dq_t: the joint velocity trajectory, stored in a Curves object
+        """
+        self.q_t = q_t
+        self.dq_t = dq_t
+        self.t = q_t.min()
+        self.t_max = q_t.max()
+        super().__init__(period)
+
+    def loop(self, signum, frame):
+        self.t += self.period
+        if self.t > self.t_max:
+            self.stop()
+
+        # Get position/orientation of the base and angular position of actuators
+        pyb_sim.retrieve_pyb_data()
+
+        # update camera position to follow the root position
+        camera_follow(pyb_sim.baseState[0])
+
+        # Set control for all joints
+        pyb.setJointMotorControlArray(pyb_sim.robotId, pyb_sim.revoluteJointIndices,
+                                      controlMode=pyb.POSITION_CONTROL,
+                                      targetPositions = self.q_t(self.t)[7:],
+                                      targetVelocities = self.dq_t(self.t)[6:])
+
+        # Compute one step of simulation
+        pyb.stepSimulation()
+
 
 
 if __name__ == "__main__":
@@ -36,6 +77,7 @@ if __name__ == "__main__":
     # Get time interval from planning:
     t_min = q_t.min()
     t_max = q_t.max()
+    print("## Complete duration of the motion loaded: ", t_max - t_min)
 
     # Sanity checks:
     assert t_min < t_max
@@ -59,25 +101,11 @@ if __name__ == "__main__":
     pyb.resetDebugVisualizerCamera(cameraDistance=0.8, cameraYaw=-30, cameraPitch=-35,
                                    cameraTargetPosition=[root_init[0], root_init[1], 0.1])
 
-    t = t_min
-    while t < t_max:
-        # Get position/orientation of the base and angular position of actuators
-        pyb_sim.retrieve_pyb_data()
+    # Start the control loop:
+    SimulatorLoop(DT, q_t, dq_t)
 
-        camera_follow(pyb_sim.baseState[0])
-
-        # Vector that contains torques
-        q = q_t(t)[7:]
-        dq = dq_t(t)[6:]
-        ddq = ddq_t(t)[6:]
-        tau = tau_t(t)
-
-        # Set control for all joints
-        pyb.setJointMotorControlArray(pyb_sim.robotId, pyb_sim.revoluteJointIndices,
-                                      controlMode=pyb.POSITION_CONTROL, targetPositions = q, targetVelocities = dq)
-
-        # Compute one step of simulation
-        pyb.stepSimulation()
-        #input()
-        time.sleep(DT) # TODO: use LOOP helper class from mlp instead
-        t += DT
+    # compute final position error of the base
+    root_final_desired = cs.contactPhases[-1].q_t(cs.contactPhases[-1].timeFinal)[:2]
+    pyb_sim.retrieve_pyb_data()
+    root_final = pyb_sim.baseState[0][:2]
+    print("## Final position error of the base (x, y) :", root_final_desired - root_final)
