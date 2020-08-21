be8975c0a1a2c269aad01564aa36e73feb3547aa
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-08-21 13:48:11 +0200

    fix missing imports

--- pyb_solo_simulator.py
+++ pyb_solo_simulator.py
@@ -7,6 +7,9 @@ from .loop import Loop
 
 class pybullet_simulator:
     def __init__(self, dt=0.001, q_init=None, root_init = None, env_name="plane.urdf", env_package = None, urdf_name="solo12.urdf", force_control=False, use_gui = True):
+        
+        import pybullet as opyb
+        import pybullet_data        
 
         self.ENV_NAME = env_name
         self.ROBOT_URDF_NAME = urdf_name
