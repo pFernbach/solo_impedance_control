fde44c4396b6a08f8d76d83d65d874ca37a77409
Pierre Fernbach <pierre.fernbach@gmail.com>
2020-09-02 11:23:02 +0200

    pyb_solo_simulator: add torque control option

--- pyb_solo_simulator.py
+++ pyb_solo_simulator.py
@@ -3,7 +3,9 @@ import numpy as np
 from pybullet_utils import bullet_client
 
 from example_robot_data.robots_loader import getModelPath
-from .loop import Loop
+from loop import Loop
+
+SOLO8=True
 
 class pybullet_simulator:
     def __init__(self, dt=0.001, q_init=None, root_init = None, env_name="plane.urdf", env_package = None, urdf_name="solo12.urdf", force_control=False, use_gui = True):
@@ -62,7 +64,7 @@ class pybullet_simulator:
             jointTorques = [0.0 for m in self.revoluteJointIndices]
             self.pyb.setJointMotorControlArray(self.robotId,
                                           self.revoluteJointIndices,
-                                          controlMode=pyb.TORQUE_CONTROL,
+                                          controlMode=self.pyb.TORQUE_CONTROL,
                                           forces=jointTorques)
 
         # Set time step for the simulation
@@ -114,7 +116,7 @@ class pybullet_simulator:
 
 
 
-class SimulatorLoop(Loop):
+class VelocityControlLoop(Loop):
     """
     Class used to call pybullet at a given frequency
     """
@@ -146,11 +148,101 @@ class SimulatorLoop(Loop):
                                       controlMode=self.pyb_sim.pyb.POSITION_CONTROL,
                                       targetPositions=self.q_t(self.t)[7:],
                                       targetVelocities=self.dq_t(self.t)[6:])
+        if SOLO8:
+            for i in range(4):
+                self.pyb_sim.pyb.setJointMotorControl2(self.pyb_sim.robotId,
+                                          self.pyb_sim.revoluteJointIndices[i*3],
+                                          controlMode=self.pyb_sim.pyb.VELOCITY_CONTROL,
+                                          targetVelocity=0.)  
         # Compute one step of simulation
         self.pyb_sim.pyb.stepSimulation()
         
         # Get position/orientation of the base and angular position of actuators
         self.pyb_sim.retrieve_pyb_data()
+        # update camera position to follow the root position
+        if self.pyb_sim.use_gui:
+            self.pyb_sim.camera_follow(self.pyb_sim.baseState[0])
+
+        # display the motion with display_func if required:
+        if self.display_func is not None and self.pyb_sim.qmes12 is not None:
+            self.display_func(self.pyb_sim.qmes12.reshape(-1,1))
+
+
+
+
+class TorqueControlLoop(Loop):
+    """
+    Class used to call pybullet at a given frequency
+    """
+    def __init__(self, pyb_sim, period, q_t, dq_t, tau_t, display_func = None):
+        """
+        Constructor
+        :param pyb_sim: instance of pybullet_simulator class
+        :param period: the time step between each new frame
+        :param q_t: the joint position trajectory, stored in a Curves object
+        :param dq_t: the joint velocity trajectory, stored in a Curves object
+        :param display_func: function pointer, if provided this function is called
+        with the wholebody configuration (including free-flyer) after each simulation step
+        """
+        self.pyb_sim = pyb_sim
+        self.display_func = display_func
+        self.q_t = q_t
+        self.dq_t = dq_t
+        self.tau_t = tau_t
+        self.t = q_t.min()
+        self.t_max = q_t.max()
+        self.kd = np.ones([12,1]) * 0.3
+        for i in range(4):
+            self.kd[i*3] = 1.
+        self.kp = np.ones([12,1]) * 3.
+        super().__init__(period)
+
+    def loop(self, signum, frame):
+        self.t += self.period
+        if self.t > self.t_max:
+            self.stop()
+        # get current joint positions and velocities:
+        self.pyb_sim.retrieve_pyb_data()
+        q = self.pyb_sim.qmes12[7:]
+        v = self.pyb_sim.vmes12[6:]
+        #print("q = ", q)
+        #print("size q : ", q.shape)
+        #print("size v : ", v.shape)
+        # compute error from with the reference:
+        desired_q = self.q_t(self.t)[7:].reshape(-1,1)
+        desired_v = self.dq_t(self.t)[6:].reshape(-1,1)
+        desired_tau = self.tau_t(self.t).reshape(-1,1)
+        #print("desired size q : ", desired_q.shape)
+        #print("desired size v : ", desired_v.shape)
+        #print("desired size tau : ", desired_tau.shape)
+        pos_error = desired_q - q
+        vel_error = desired_v - v
+        #print("pos error shape : ", pos_error.shape)
+        #print("pos error : ", pos_error)
+        #print("vel error : ", vel_error)
+        tau = desired_tau + self.kp * pos_error + self.kd * vel_error
+        if  np.linalg.norm(tau) > 10:
+            print("large torque !!! :", tau)
+            print("pos error : ", pos_error)
+            print("vel error : ", vel_error)
+            print("desired torque : ", desired_tau)
+            self.stop()
+        #tau = desired_tau
+        #print("command size tau : ", tau.shape)
+        # Set control for all joints
+        self.pyb_sim.pyb.setJointMotorControlArray(self.pyb_sim.robotId,
+                                      self.pyb_sim.revoluteJointIndices,
+                                      controlMode=self.pyb_sim.pyb.TORQUE_CONTROL,
+                                      forces=tau)
+        if SOLO8:
+            for i in range(4):
+                self.pyb_sim.pyb.setJointMotorControl2(self.pyb_sim.robotId,
+                                          self.pyb_sim.revoluteJointIndices[i*3],
+                                          controlMode=self.pyb_sim.pyb.VELOCITY_CONTROL,
+                                          targetVelocity=0.)  
+        # Compute one step of simulation
+        self.pyb_sim.pyb.stepSimulation()
+        
         # update camera position to follow the root position
         if self.pyb_sim.use_gui:
             self.pyb_sim.camera_follow(self.pyb_sim.baseState[0])
